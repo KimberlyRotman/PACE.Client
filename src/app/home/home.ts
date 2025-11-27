e23de6e899dd18a7c1705b17f9627738610f4d6b
Kimberly Santos <kimberly.santos@volvo.com>
2025-11-27 10:44:14 -0300

    Alteração de Design página inicial, login e cadastro

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { Login } from "../login/login";
 
 @Component({
   selector: 'app-home',
-  imports: [ MatButtonModule, MatIconModule, RouterModule ],
+  imports: [MatButtonModule, MatIconModule, RouterModule, Login],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
